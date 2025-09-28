f058da352b5ce4d3b191a7958f35869d0a42fea1
Faisal0385 <faisaltez@gmail.com>
2025-09-28 18:37:51 +0600

    updated

---
+++ public/admin/assets/css/config.js
@@ -0,0 +1,33 @@
+function showLoader() {
+    document.getElementById('loader').classList.remove('d-none')
+}
+
+function hideLoader() {
+    document.getElementById('loader').classList.add('d-none')
+}
+
+function successToast(msg) {
+    Toastify({
+        gravity: "bottom", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        text: msg,
+        className: "mb-5",
+        style: {
+            background: "green",
+            borderRadius: "5px"
+        }
+    }).showToast();
+}
+
+function errorToast(msg) {
+    Toastify({
+        gravity: "bottom", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        text: msg,
+        className: "mb-5",
+        style: {
+            background: "red",
+            borderRadius: "5px"
+        }
+    }).showToast();
+}
